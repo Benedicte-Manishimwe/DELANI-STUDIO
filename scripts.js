3bbc79c397621b9146e4d70b601b861b01ab839b
Benedicte-Manishimwe <benoite251@gmail.com>
2019-07-26 17:35:14 +0200

    style in jQuery whatdo div

--- scripts.js
+++ scripts.js
@@ -1,26 +1,27 @@
 $(document).ready(function(){
-  $("toDo-1").click(function(){
-    $("toDo-1").toggle();
+  $("toDo-1").click(function(){ 
     $("text-1").toggle();
+    $("toDo-1").toggle();
   });
   $("text-1").click(function(){
-   $("text-1").toggle();
-   $("toDo-1").toggle();
+     $("toDo-1").toggle();
+     $("text-1").toggle();
+  
   });
   $("toDo-2").click(function(){
-    $("toDo-2").toggle();
     $("text-2").toggle();
+    $("toDo-2").toggle();
   });
   $("text-2").click(function(){
-    $("text-2").toggle();
     $("toDo-2").toggle();
+    $("text-2").toggle();
   });
   $("toDo-3").click(function(){
-    $("toDo-3").toggle();
     $("text-3").toggle();
+    $("toDo-3").toggle();
   });
   $("text-3").click(function(){
-  $("text-3").toggle();
-  $("toDo-3").toggle();
+    $("toDo-3").toggle();
+    $("text-3").toggle();
   });
 });
