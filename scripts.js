e0b0f82c279ca8c10d4ab4ece25715a4270ef852
Benedicte-Manishimwe <benoite251@gmail.com>
2019-07-27 19:10:13 +0200

    set the input of forms

--- scripts.js
+++ scripts.js
@@ -1,36 +1,54 @@
-$(document).ready(function(){
-  $("toDo-1").click(function(){ 
-    $("text-1").toggle();
-    $("toDo-1").toggle();
+$(document).ready(function () {
+  $("#toDo-1").click(function () {
+    $("#text-1").toggle();
+    $("#toDo-1").toggle();
   });
-  $("text-1").click(function(){
-     $("toDo-1").toggle();
-     $("text-1").toggle();
-  
+  $("#text-1").click(function () {
+    $("#toDo-1").toggle();
+    $("#text-1").toggle();
+
   });
-  $("toDo-2").click(function(){
-    $("text-2").toggle();
-    $("toDo-2").toggle();
+  $("#toDo-2").click(function () {
+    $("#text-2").toggle();
+    $("#toDo-2").toggle();
   });
-  $("text-2").click(function(){
-    $("toDo-2").toggle();
-    $("text-2").toggle();
+  $("#text-2").click(function () {
+    $("#toDo-2").toggle();
+    $("#text-2").toggle();
   });
-  $("toDo-3").click(function(){
-    $("text-3").toggle();
-    $("toDo-3").toggle();
+  $("#toDo-3").click(function () {
+    $("#text-3").toggle();
+    $("#toDo-3").toggle();
   });
-  $("text-3").click(function(){
-    $("toDo-3").toggle();
-    $("text-3").toggle();
+  $("#text-3").click(function () {
+    $("#toDo-3").toggle();
+    $("#text-3").toggle();
   });
-});
-$(document).ready(function(){
-  $("work-4").click(function(){
-    $("work-4").hover(function(){
-      $("des-4").show();
-    });
+
+
+  $("#work4").mouseover(function(){
+    $("#pres-4").show();
+    // $("#work-4").addClass(".pres-4");
   });
+  $("#work4").mouseleave(function(){
+   $("#pres-4").hide();
+   });
+
+
+  $("#valid").click(function () {
+    var input1=$("#names").value;
+    var input2=$("#emails").value;
+    var input3=$("#textarea").value;
+
+    if ((input1=="")||(input2=="") || (input3=="")) {
+      alert("Please fill the blank spaces!!");
+    }
+    else {
+      alert(input1 + " , we have received your message. Thank you fro reaching out to us.");
+    }
+
+  })
+
 });
 
 
