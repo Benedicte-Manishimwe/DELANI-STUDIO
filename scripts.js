785002db79f8ae0b3e4434a0f4f3161156f3aed8
Benedicte-Manishimwe <benoite251@gmail.com>
2019-07-29 13:35:19 +0200

    make the page responsive

--- scripts.js
+++ scripts.js
@@ -48,35 +48,35 @@ $(document).ready(function () {
     $(".pres6").hide();
   });
   $("#wk5").mouseover(function(){
-    $(".pres5").css("background-color","rgba(255,255,255,0.4");
+    $(".pres5").css("background-color","rgba(255,255,255,0.4)");
     $(".pres5").fadeIn();
   });
   $("#wk5").mouseleave(function(){
     $(".pres5").hide();
   });
   $("#wk4").mouseover(function(){
-    $(".pres4").css("background-color","rgba(255,255,255,0.4");
+    $(".pres4").css("background-color","rgba(255,255,255,0.4)");
     $(".pres4").fadeIn();
   });
   $("#wk4").mouseleave(function(){
     $(".pres4").hide();
   });
   $("#wk3").mouseover(function(){
-    $(".pres3").css("background-color","rgba(255,255,255,0.4");
+    $(".pres3").css("background-color","rgba(255,255,255,0.4)");
     $(".pres3").fadeIn();
   });
   $("#wk3").mouseleave(function(){
     $(".pres3").hide();
   });
   $("#wk2").mouseover(function(){
-    $(".pres2").css("background-color","rgba(255,255,255,0.4");
+    $(".pres2").css("background-color","rgba(255,255,255,0.4)");
     $(".pres2").fadeIn();
   });
   $("#wk2").mouseleave(function(){
     $(".pres2").hide();
   });
   $("#wk1").mouseover(function(){
-    $(".pres1").css("background-color","rgba(255,255,255,0.4");
+    $(".pres1").css("background-color","rgba(255,255,255,0.4)");
     $(".pres1").fadeIn();
   });
   $("#wk1").mouseleave(function(){
@@ -89,7 +89,7 @@ $(document).ready(function () {
     var input3=$("#textarea").val();
 
     if ((input1==="")||(input2==="") || (input3==="")){
-      alert("Please fill the blank spaces!!");
+      alert("Please ,fill the blank spaces for names emails and messages !!");
     }
     else {
       alert(input1 + " , we have received your message. Thank you fro reaching out to us.");
